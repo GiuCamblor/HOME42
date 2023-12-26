9b1a7fa356fa1fb7c21b9c06fc94764c2519320a
Giu Camblor <gcamblor@c4r3s6.42urduliz.com>
2023-12-26 16:45:57 +0100

    cambio puesto

--- PISCINE RELOADED/ex09/ft_ft.c
+++ PISCINE RELOADED/ex09/ft_ft.c
@@ -1,10 +1,19 @@
+
 #include <unistd.h>
+#include <stdio.h>
 
+void ft_ft(int *nbr)
+{
+    *nbr = 42;
+    printf ("Valor 2: %i\n", *nbr);
+}
 
-int main(int)
+int main(void)
 {
     int num;
     num = 0;
-    write (1, num, 1);
+    printf ("Valor 1: %i\n", num);
+    ft_ft(&num);
+    printf ("Valor 2: %i\n", num);
     return (0);
 }
