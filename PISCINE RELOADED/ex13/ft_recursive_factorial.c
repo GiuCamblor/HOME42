c8e70cf3a5fd3c8f492e9f2bad1076ec3cf82555
Giu Camblor <gcamblor@c4r1s5.42urduliz.com>
2023-12-27 18:50:51 +0100

    ex13 wip / ex12 wip_campus

--- PISCINE RELOADED/ex13/ft_recursive_factorial.c
+++ PISCINE RELOADED/ex13/ft_recursive_factorial.c
@@ -1,39 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcamblor <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/26 18:46:41 by gcamblor          #+#    #+#             */
-/*   Updated: 2023/12/26 18:47:02 by gcamblor         ###   ########.fr       */
+/*   Created: 2023/12/27 18:21:43 by gcamblor          #+#    #+#             */
+/*   Updated: 2023/12/27 18:21:50 by gcamblor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-int ft_iterative_factorial(int nb)
+int ft_recursive_factorial(int nb)
 {
-	int fact;
 	int res;
-
-	fact = nb;
-
-		while (--fact > 1)
-		{
-			nb *= fact;
-		}
-	return (nb);
+	if (nb == 1)
+	{
+		return(1);
+	}
+	res = nb * ft_recursive_factorial(nb -1);
+	return(res);
 }
 
-int	main()
+int main()
 {
 	int num;
-	int result;
+	int res;
 
-	num = 4;
-	result = ft_iterative_factorial(num);
-	printf("fact of %i is %i", num, result);
+	num = 11
+;
+	res = ft_recursive_factorial(num);
+	printf("fact of %i is %i\n", num, res);
 	return (0);
 }
