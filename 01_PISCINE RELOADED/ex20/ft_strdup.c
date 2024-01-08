380775297ddc247f616194a918a5c58f915bbd8d
Giu Camblor <giucamblor@MacBook-Air-de-Giu.local>
2024-01-08 17:24:43 +0100

    ex20 home

---
+++ 01_PISCINE RELOADED/ex20/ft_strdup.c
@@ -0,0 +1,59 @@
+
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdio.h>
+
+int	ft_strlen(const char *str)
+{
+    int len;
+
+	while (str[len] != '\0')
+		len++;
+    return(len);
+}
+
+char *ft_strdup(const char *src, int len)
+{
+    char *copy;
+    copy = (char *)malloc((len + 1) * sizeof(char));
+    if (copy != NULL) {
+        for (int i = 0; i <= len; i++) {
+            copy[i] = src[i];
+        }
+    }
+
+    return copy;
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(const char *str)
+{
+	char i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int	main(void)
+{
+	const char *str;
+    char *copy;
+    int len;
+
+    str = "CADENA";
+    len = ft_strlen(str);
+    copy = ft_strdup(str, len);
+    ft_putstr(str);
+
+	str = "CADENA";
+    printf("\n");
+	return(0);
+}
