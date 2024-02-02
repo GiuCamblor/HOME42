9915a152c4ecf3fbbe6cc1e5cea2ab84eb832ef3
Giu Camblor <giucamblor@MacBook-Air-de-Giu.local>
2024-02-02 12:37:14 +0100

    ft_split.c WIP HOME

--- 02_LIBFT/Libft/ft_split.c
+++ 02_LIBFT/Libft/ft_split.c
@@ -1,23 +1,38 @@
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/* ************************************************************************** */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gcamblor <gcamblor@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:42:04 by gcamblor          #+#    #+#             */
-/*   Updated: 2024/01/31 22:57:11 by gcamblor         ###   ########.fr       */
+/*   Updated: 2024/02/02 12:26:48 by gcamblor         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "libft.h"
 #include <stdio.h> 
-
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*af_substr(char const *s, unsigned int start, size_t len);
 size_t	af_substrcnt(char const *s, char c);
 //char	*faf_fillArrayWords(char *arr, const char *s, int s_ind, char c);
 char	*af_substrfill(char const *s, int s_ind, int w_ind, char c);
+char	*af_substr(char const *s, unsigned int start, size_t len)		////////////FUNCIÓN AUXILIAR SUSTITUIDA POR ft_substr
+{
+	char	*d;
+
+	if (start > ft_strlen(s) || !s || len == 0)
+		return (ft_strdup(""));
+	if (ft_strlen((char *)s) - start < len)
+		len = ft_strlen((char *)s) - start;
+	d = (char *) malloc(sizeof(char) * (len + 1));
+	if (!d)
+		return (0);
+	ft_memcpy(d, s + start, len);
+	d[len] = '\0';
+	return (d);
+}
 /*
-char	*fun_fillArrayWords(char *arr, const char *s, int s_ind, char c)		////////////FUNCIÓN AUXILIAR SUSTITUIDA POR ft_substr
 {																							(Al sacar la resta s_ind = s_ind - w_ind para no superar los cuatro argumentos, había que recuperar el valor después de esta función o de ft_substr---COMENTAR)
 	int w_ind;
 	//s_ind = s_ind - w_ind;
@@ -65,15 +80,17 @@ char	*af_substrfill(char const *s, int s_ind, int substr_ind, char c)
  		while (s[s_ind] == c)
 			s_ind++;
 		substr_ind = 0;
-		while (s[s_ind] && s[substr_ind] != c)
+		while (s[s_ind] && s[s_ind] == c)
 		{
 			s_ind++;
-			substr_ind++;
 		}
-		s_ind = s_ind - substr_ind;
-		arr = ft_substr(s, s_ind, substr_ind);
+printf("SIND: %d\n", s_ind);
+printf("SUBSTRIND: %d\n", substr_ind);
+printf("STRING: %s\n", s);
+		arr = af_substr(s, s_ind, substr_ind);
+printf("SUBSTRING: %s\n", arr);
+printf("SUBSTR_LEN_IN FILL: %zu\n", ft_strlen(arr));
 		s_ind = s_ind + substr_ind;
-		printf("%i\n", s_ind);
 		return (arr);
 }	
 
@@ -93,13 +110,15 @@ char	**ft_split(char const *s, char c)
 		return (0);
 	while (substr_cnt < af_substrcnt(s, c))
 	{	
+		while (s[s_ind] == c)
+			s_ind++;
 		arr[substr_cnt] = af_substrfill(s, s_ind, substr_ind, c);
-		s_ind = s_ind - ft_strlen(arr[substr_cnt]);
-		//s_ind = s_ind + substr_ind;
-		printf("PALABRA %i: %s\n", substr_cnt + 1, arr[substr_cnt]);
+		s_ind = s_ind + ft_strlen(arr[substr_cnt]);
+//printf("%i\n", s_ind);
+//printf("SUBSTR_LEN: %zu\n", ft_strlen(arr[substr_cnt]));
+//printf("SUBSTRINGS: %i\n", substr_cnt);
+//printf("SUBSTRING %i: %s\n", substr_cnt + 1, arr[substr_cnt]);
 		substr_cnt++;
 	}
-	printf("SUBSTRINGS: %i\n", substr_cnt);
-	//arr[word_cnt] = NULL;
 	return (arr);
 }
